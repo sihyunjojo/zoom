357cb2949ecdcc98767f166fb3068e14fd6b92c5
sihyunjojo <si4018@naver.com>
2022-03-15 23:07:55 +0900

    video start

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,64 +1,9 @@
-const socket = io() /* 알아서 socket.io를 찾아서 실행시킴 */
-    /* socket의 정보에서 socket의 id를 볼 수 있다. */
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const socket = io();
 
-room.hidden = true;
+const myFace = document.getElementById("myFace");
 
-let roomName = room.innerHTML;
+let myStream;
 
-function addMessage(message) {
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
+async function getMedia() {
+    try {}
 }
-
-function handleMessageSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value; // 만약 이 줄을 사용하지 않고 마지막에서 input.value를 지워주게 되면 you: ${input.value}코드 실행시에 you 이후의 인자값을 비워진 채로 받아온다.
-    socket.emit("new_message", input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
-
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    const value = input.value;
-    socket.emit("nickname", input.value);
-}
-
-function showRoom() {
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`
-    const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
-    msgForm.addEventListener("submit", handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNickSubmit);
-}
-
-function handleRoomSubmit(event) {
-    event.preventDefault();
-    const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, showRoom); /* 마지막 arguement에는 반드시 보내고 싶은 함수가 와야함. */
-    roomName = input.value;
-    input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-
-socket.on("welcome", (user) => {
-    addMessage(`${user} arrived~~`);
-});
-
-socket.on("bye", (left) => {
-    addMessage(`${left} left ㅠㅠ`);
-});
-/* socket.on("welcome", addMessage("someone joined!")); */
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
+import { SocketAddress } from "net";
 
 const app = express();
 
@@ -13,53 +15,12 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
-
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "Anon";
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-    });
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName); //방이 있어야함.  방만듬
-        done();
-        /* backend에서 실행시키는 것이 아니라 done 함수를 실행시키면 front에서 함수를 실행시킴 
-         front에서 실행 버튼을 눌러주면 front에 있는 이 함수를 backend가 실행시킨다. */
-        socket.to(roomName).emit("welcome", socket.nickname);
-        //welcome이라는 이벤트를 roomname에 있는 모든 사람들에게 emit를 한것이다.
-
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach(room =>
-            socket.to(room).emit("bye", socket.nickname)
-        );
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done(); //이 코드는 백앤드에서 실행되지 않는다 프론트에서 코드를 실행된다.
-    });
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
 });
-
-function onSocketClose() {
-    console.log("Disconnected from the Browswer X");
-}
-
-const sockets = [];
-
-
-httpServer.listen(3000, handleListen);
-
-
-
-{
-    type: "message"
-    payload: "hello everyone!"
-}
-
-{
-    type: "nickname"
-    payload: "sihyun"
-}
-/* 
-json */
+instrument(wsServer, {
+    auth: false
+});
